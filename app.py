0ae703692ebddd5b06c43bdc3dd436a03918b24b
jemoo <wpan1060@naver.com>
2022-03-09 16:52:00 +0900

    세번째

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'miniExhibition'
 @app.route('/')
 def home():
     rows = post_list = list(db.post.find({}, {'_id': False}))
-    return render_template('index.html', rows= rows)
+    return render_template('index.html', rows=rows)
 
 
 # 회원가입
@@ -75,6 +75,7 @@ def show_images():
 
     return jsonify({'images': image_list})
 
+
 # 로그인 페이지 이동
 @app.route('/loginpage')
 def loginPage():
@@ -87,11 +88,32 @@ def postingPage():
     return render_template("posting.html")
 
 
+# 상세 글 페이지 이동
+@app.route('/detailPost/<keyword>')
+def detailPage(keyword):
+    postInfo = db.post.find_one({'post_num': int(keyword)}, {'_id': False})
+    comment_list = list(db.comment.find({'post_num': int(keyword)}, {'_id': False}))
+
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_id = payload["id"]
+
+        if user_id == postInfo['writer_id']:
+            idCheck = True
+        else:
+            idCheck = False
+
+        return render_template('detail.html', post_info=postInfo, comment_info=comment_list, id_check=idCheck)
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
 # 글 등록
 @app.route("/posting/post", methods=["POST"])
 def upload_post():
     token_receive = request.cookies.get('mytoken')
-    try :
+    try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
         user_id = payload["id"]
@@ -101,14 +123,14 @@ def upload_post():
 
         url_list = list(db.post.find({}, {'_id': False}).distinct('post_num'))  # distinct는 key 값 생략하고 value 값만 가져오기
 
-        if len(url_list) == 0 :
+        if len(url_list) == 0:
             count = 1
-        else :
+        else:
             count = max(url_list) + 1
 
         doc = {
             'post_num': count,
-            'writer_id' : user_id,
+            'writer_id': user_id,
             'url': url_receive,
             'pic_name': pic_name_receive,
             'pic_explain': pic_explain_receive
@@ -116,7 +138,72 @@ def upload_post():
 
         db.post.insert_one(doc)
         return jsonify({'msg': '등록완료!'})
-    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError) :
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+# 글 삭제
+@app.route("/post_cancel", methods=["POST"])
+def post_cancel():
+
+    post_num_receive = request.form['post_num_give']
+    db.post.delete_one({'post_num': int(post_num_receive)})
+
+    return jsonify({'msg': '삭제되었습니다'})
+
+
+# 댓글 등록
+@app.route("/comment", methods=["POST"])
+def upload_comment():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        user_id = payload["id"]
+        post_num = request.form['post_num_give']
+        comment_text = request.form['comment_give']
+
+        comment_list = list(db.post.find({'post_num': post_num}, {'_id': False}).distinct(
+            'comment_num'))  # distinct는 key 값 생략하고 value 값만 가져오기
+
+        if len(comment_list) == 0:
+            count = 1
+        else:
+            count = max(comment_list) + 1
+
+        doc = {
+            'post_num': int(post_num),
+            'comment_num': count,
+            'comment_writer_id': user_id,
+            'comment_text': comment_text
+        }
+
+        db.comment.insert_one(doc)
+
+        return jsonify({'msg': '등록완료!'})
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+# 댓글 삭제
+@app.route("/comment_cancel_check", methods=["POST"])
+def cancel_check():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        user_id = payload["id"]
+        comment_writer_id_receive = request.form['comment_writer_id_give']
+        post_num_receive = request.form['post_num_give']
+        comment_num_receive = request.form['comment_num_give']
+
+        if user_id == comment_writer_id_receive:
+            db.comment.delete_one({'post_num': int(post_num_receive), 'comment_num': int(comment_num_receive)})
+            check = True
+        else:
+            check = False
+        return jsonify({'check': check})
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
 
